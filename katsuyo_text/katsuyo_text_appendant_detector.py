c09386532ffb3bdda71c4ec906f4f9219f448d7d
sadahiroyoshi <sadahiroyoshi@gmail.com>
2022-11-08 23:59:36 +0900

    refactor: divide source/appendant

--- katsuyo_text/katsuyo_text_appendant_detector.py
+++ katsuyo_text/katsuyo_text_appendant_detector.py
@@ -1,35 +1,12 @@
 from typing import Any, Optional, List, Set, Tuple, Type
 from itertools import dropwhile
-from katsuyo_text.katsuyo import (
-    GODAN_BA_GYO,
-    GODAN_GA_GYO,
-    GODAN_IKU,
-    GODAN_KA_GYO,
-    GODAN_MA_GYO,
-    GODAN_NA_GYO,
-    GODAN_RA_GYO,
-    GODAN_SA_GYO,
-    GODAN_TA_GYO,
-    GODAN_WAA_GYO,
-    KAMI_ICHIDAN,
-    KEIYOUDOUSHI,
-    KEIYOUSHI,
-    SA_GYO_HENKAKU_SURU,
-    SA_GYO_HENKAKU_ZURU,
-    SHIMO_ICHIDAN,
-)
 from katsuyo_text.katsuyo_text import (
-    KURU,
-    KURU_KANJI,
     FukujoshiText,
     KatsuyoTextErrorMessage,
     KatsuyoTextHasError,
     IKatsuyoTextAppendant,
-    IKatsuyoTextSource,
-    KatsuyoText,
     KatsuyoTextError,
     ShujoshiText,
-    TaigenText,
     FUKUZYOSHI_BAKARI,
     FUKUZYOSHI_MADE,
     FUKUZYOSHI_DAKE,
@@ -68,19 +45,7 @@ import warnings
 import spacy
 
 
-class IKatsuyoTextSourceDetector(abc.ABC):
-    @abc.abstractmethod
-    def detect(self, src: Any) -> Optional[IKatsuyoTextSource]:
-        """
-        不適切な値が代入された際は、Noneを返却する。
-        """
-        raise NotImplementedError()
-
-    def __str__(self):
-        return self.__class__.__name__
-
-
-class IKatsuyoTextAppendantsDetector(abc.ABC):
+class IKatsuyoTextAppendantDetector(abc.ABC):
     SUPPORTED_HELPERS = (
         Ukemi,
         Shieki,
@@ -179,153 +144,7 @@ class IKatsuyoTextAppendantsDetector(abc.ABC):
         return self.__class__.__name__
 
 
-class SpacyKatsuyoTextSourceDetector(IKatsuyoTextSourceDetector):
-    VERB_KATSUYOS_BY_CONJUGATION_TYPE = {
-        "五段-カ行": GODAN_KA_GYO,
-        "五段-ガ行": GODAN_GA_GYO,
-        "五段-サ行": GODAN_SA_GYO,
-        "五段-タ行": GODAN_TA_GYO,
-        "五段-ナ行": GODAN_NA_GYO,
-        "五段-バ行": GODAN_BA_GYO,
-        "五段-マ行": GODAN_MA_GYO,
-        "五段-ラ行": GODAN_RA_GYO,
-        "五段-ワア行": GODAN_WAA_GYO,
-        "上一段-ア行": KAMI_ICHIDAN,
-        "上一段-カ行": KAMI_ICHIDAN,
-        "上一段-ガ行": KAMI_ICHIDAN,
-        "上一段-ザ行": KAMI_ICHIDAN,
-        "上一段-タ行": KAMI_ICHIDAN,
-        "上一段-ナ行": KAMI_ICHIDAN,
-        "上一段-バ行": KAMI_ICHIDAN,
-        "上一段-マ行": KAMI_ICHIDAN,
-        "上一段-ラ行": KAMI_ICHIDAN,
-        "下一段-ア行": SHIMO_ICHIDAN,
-        "下一段-カ行": SHIMO_ICHIDAN,
-        "下一段-ガ行": SHIMO_ICHIDAN,
-        "下一段-サ行": SHIMO_ICHIDAN,
-        "下一段-ザ行": SHIMO_ICHIDAN,
-        "下一段-タ行": SHIMO_ICHIDAN,
-        "下一段-ダ行": SHIMO_ICHIDAN,
-        "下一段-ナ行": SHIMO_ICHIDAN,
-        "下一段-ハ行": SHIMO_ICHIDAN,
-        "下一段-バ行": SHIMO_ICHIDAN,
-        "下一段-マ行": SHIMO_ICHIDAN,
-        "下一段-ラ行": SHIMO_ICHIDAN,
-    }
-
-    def detect(self, src: spacy.tokens.Token) -> Optional[IKatsuyoTextSource]:
-        # spacy.tokens.Tokenから抽出される活用形の特徴を表す変数
-        tag = src.tag_
-        lemma = src.lemma_
-        # sudachiの形態素解析結果(part_of_speech)5つ目以降(活用タイプ、活用形)が格納される
-        # 品詞によっては活用タイプ、活用形が存在しないため、ここでは配列の取得のみ行う
-        # e.g. 動詞
-        # > m.part_of_speech() # => ['動詞', '一般', '*', '*', '下一段-バ行', '連用形-一般']
-        # ref. https://github.com/explosion/spaCy/blob/v3.4.1/spacy/lang/ja/__init__.py#L102
-        # ref. https://github.com/WorksApplications/SudachiPy/blob/v0.5.4/README.md
-        # > Returns the part of speech as a six-element tuple. Tuple elements are four POS levels, conjugation type and conjugation form.
-        # ref. https://worksapplications.github.io/sudachi.rs/python/api/sudachipy.html#sudachipy.Morpheme.part_of_speech
-        inflection = src.morph.get("Inflection")
-        if len(inflection) > 0:
-            inflection = inflection[0].split(";")
-
-        # There is no VBD tokens in Japanese
-        # ref. https://universaldependencies.org/treebanks/ja_gsd/index.html#pos-tags
-        # if pos_tag == "VBD":
-
-        if tag.startswith("動詞"):
-            # ==================================================
-            # 動詞の判定
-            # ==================================================
-            # 「いく」は特殊な変形
-            if lemma in ["行く", "逝く", "往く", "征く"]:
-                return KatsuyoText(gokan=lemma[:-1], katsuyo=GODAN_IKU)
-            elif lemma in ["いく", "ゆく"]:
-                # 「ゆく」も「いく」に含める（過去・完了「た」を「ゆった」「ゆいた」とはできないため）
-                return KatsuyoText(gokan="い", katsuyo=GODAN_IKU)
-
-            # 活用タイプを取得して判定に利用
-            assert inflection, f"inflection is not empty: {src}"
-            conjugation_type = inflection[0]
-
-            # 活用形の判定
-            katsuyo = self.VERB_KATSUYOS_BY_CONJUGATION_TYPE.get(conjugation_type)
-            if katsuyo:
-                return KatsuyoText(gokan=lemma[:-1], katsuyo=katsuyo)
-
-            # 例外的な活用形の判定
-            if conjugation_type == "カ行変格":
-                # カ変「くる」「来る」を別途ハンドリング
-                if lemma == "来る":
-                    return KURU_KANJI
-                elif lemma == "くる":
-                    return KURU
-            elif conjugation_type == "サ行変格":
-                # サ変「する」「ずる」を別途ハンドリング
-                if lemma[-2:] == "する":
-                    return KatsuyoText(gokan=lemma[:-2], katsuyo=SA_GYO_HENKAKU_SURU)
-                elif lemma[-2:] == "ずる":
-                    return KatsuyoText(gokan=lemma[:-2], katsuyo=SA_GYO_HENKAKU_ZURU)
-
-            warnings.warn(
-                f"Unsupported conjugation_type of VERB: {conjugation_type}", UserWarning
-            )
-            return None
-        elif tag.startswith("形容詞"):
-            # ==================================================
-            # 形容詞の変形
-            # ==================================================
-            # e.g. 楽しい -> gokan=楽し + katsuyo=い
-            return KatsuyoText(gokan=lemma[:-1], katsuyo=KEIYOUSHI)
-        elif tag.startswith("形状詞"):
-            # ==================================================
-            # 形容動詞の変形
-            # ==================================================
-            # 「形状詞」=「形容動詞の語幹」
-            # universaldependenciesの形容動詞に語幹は含まれない
-            # see: https://universaldependencies.org/treebanks/ja_gsd/ja_gsd-pos-ADJ.html
-            # e.g. 健康 -> gokan=健康 + katsuyo=だ
-            return KatsuyoText(gokan=lemma, katsuyo=KEIYOUDOUSHI)
-        elif tag.startswith("名詞"):
-            # ==================================================
-            # 例外：サ変動詞の判定
-            # ==================================================
-            # 体言を含むサ変動詞の判定
-            # 名詞がVERBとして現れるため、conjugation_typeが取得できない
-            # # text = ウォーキングする
-            # 1       ウォーキング    ウォーキング    VERB    名詞-普通名詞-一般      _       0       root    _       SpaceAfter=No|BunsetuBILabel=B|BunsetuPositionType=ROOT|Reading=ウォーキング
-            # 2       する    する    AUX     動詞-非自立可能 _       1       aux     _       SpaceAfter=No|BunsetuBILabel=I|BunsetuPositionType=SYN_HEAD|Inf=サ行変格,終止形-一般|Reading=スル
-            if src.pos_ == "VERB":
-                right = next(src.rights, None)
-                if right and right.lemma_ == "する":
-                    return KatsuyoText(gokan=lemma, katsuyo=SA_GYO_HENKAKU_SURU)
-                # このパターンは存在しない
-                # elif right.lemma_ == "ずる":
-                #     return KatsuyoText(gokan=lemma, katsuyo=SA_GYO_HENKAKU_ZURU)
-            # ==================================================
-            # 形容動詞の判定
-            # ==================================================
-            # 「形状詞」=「形容動詞の語幹」
-            # universaldependenciesの形容動詞に語幹は含まれない
-            # see: https://universaldependencies.org/treebanks/ja_gsd/ja_gsd-pos-ADJ.html
-            # e.g. 健康 -> gokan=健康 + katsuyo=だ
-            if src.pos_ == "ADJ":
-                return KatsuyoText(gokan=lemma, katsuyo=KEIYOUDOUSHI)
-            elif src.pos_ == "NOUN":
-                right = next(src.rights, None)
-                if right and right.lemma_ == "だ":
-                    return KatsuyoText(gokan=lemma, katsuyo=KEIYOUDOUSHI)
-            # ==================================================
-            # 名詞/固有名詞の変形
-            # ==================================================
-            # 名詞は形容動詞的に扱う
-            # e.g. 健康 -> gokan=健康 + katsuyo=だ
-            return TaigenText(gokan=src.text)
-
-        return None
-
-
-class SpacyKatsuyoTextAppendantsDetector(IKatsuyoTextAppendantsDetector):
+class SpacyKatsuyoTextAppendantDetector(IKatsuyoTextAppendantDetector):
     def detect_from_sent(
         self, sent: spacy.tokens.Span, src: spacy.tokens.Token
     ) -> Tuple[List[IKatsuyoTextAppendant], KatsuyoTextHasError]:
@@ -499,7 +318,7 @@ class SpacyKatsuyoTextAppendantsDetector(IKatsuyoTextAppendantsDetector):
         return self.try_get_shujoshi("のに")
 
 
-DEFAULT_APPENDANTS_DETECTOR = SpacyKatsuyoTextAppendantsDetector(
+DEFAULT_APPENDANTS_DETECTOR = SpacyKatsuyoTextAppendantDetector(
     helpers={
         Ukemi(),
         Shieki(),
---
+++ tests/test_katsuyo_text_appendant_detector.py
@@ -0,0 +1,583 @@
+import pytest
+from katsuyo_text.katsuyo_text import (
+    FUKUZYOSHI_BAKARI,
+    FUKUZYOSHI_DAKE,
+    FUKUZYOSHI_HODO,
+    FUKUZYOSHI_KA,
+    FUKUZYOSHI_KIRI,
+    FUKUZYOSHI_KURAI,
+    FUKUZYOSHI_MADE,
+    FUKUZYOSHI_NADO,
+    # FUKUZYOSHI_NARI,
+    FUKUZYOSHI_NOMI,
+    FUKUZYOSHI_YARA,
+    FUKUZYOSHI_ZUTSU,
+    SHUJOSHI_KA,
+    SHUJOSHI_KASHIRA,
+    SHUJOSHI_NA,
+    SHUJOSHI_NO,
+    SHUJOSHI_NONI,
+    KatsuyoTextError,
+)
+from katsuyo_text.katsuyo_text_appendant_detector import (
+    SpacyKatsuyoTextAppendantDetector,
+    DEFAULT_APPENDANTS_DETECTOR,
+)
+from katsuyo_text.katsuyo_text_helper import (
+    IKatsuyoTextHelper,
+    Denbun,
+    HikyoReizi,
+    KakoKanryo,
+    Keizoku,
+    KibouOthers,
+    KibouSelf,
+    Suitei,
+    Touzen,
+    Ukemi,
+    Shieki,
+    Hitei,
+    Youtai,
+)
+
+
+@pytest.fixture(scope="session")
+def spacy_appendants_detector():
+    return DEFAULT_APPENDANTS_DETECTOR
+
+
+def katsuyo_texts_appendants_detector_init_validation_error():
+    class UnsupportedHelper(IKatsuyoTextHelper):
+        def __init__(self):
+            super().__init__(bridge=lambda x: x)
+
+        def try_merge(self, _):
+            return None
+
+    with pytest.raises(KatsuyoTextError):
+        SpacyKatsuyoTextAppendantDetector((UnsupportedHelper()))
+        assert False, "UnsupportedHelper should not be accepted."
+
+
+@pytest.mark.filterwarnings("ignore:this object doesn't have helper")
+def katsuyo_texts_appendants_detector_init_warning():
+    SpacyKatsuyoTextAppendantDetector((Ukemi()))
+
+
+@pytest.mark.parametrize(
+    "text, norm, pos, expected",
+    [
+        (
+            "あなたに愛される",
+            "れる",
+            "AUX",
+            [Ukemi],
+        ),
+        (
+            "称号が与えられる",
+            "られる",
+            "AUX",
+            [Ukemi],
+        ),
+        (
+            "あなたを愛させる",
+            "せる",
+            "AUX",
+            [Shieki],
+        ),
+        (
+            "子供を寝させる",
+            "させる",
+            "AUX",
+            [Shieki],
+        ),
+        (
+            "子供を愛さない",
+            "ない",
+            "AUX",
+            [Hitei],
+        ),
+        (
+            "子供が寝ない",
+            "ない",
+            "AUX",
+            [Hitei],
+        ),
+        (
+            "宿題もせず",
+            "ず",
+            "AUX",
+            [Hitei],
+        ),
+        (
+            "子供は産まぬ",
+            # 「ぬ」のnormは「ず」になる
+            "ず",
+            "AUX",
+            [Hitei],
+        ),
+        (
+            "子供は産まん",
+            # 「ん」のnormは「ず」になる
+            "ず",
+            "AUX",
+            [Hitei],
+        ),
+        # NOTE: 「仕方ない」は一つの形容詞として抽出されるが、
+        #       「仕方がない」は文節を区切られ、否定として抽出される。
+        #
+        #       文章全体の意味を成立させるうえで問題は発生しないためBugとはしないが、
+        #       否定の意味を扱ううえでは問題となるため、改善の余地がある。
+        (
+            "それは仕方ない",
+            "仕方無い",
+            "ADJ",
+            [],
+        ),
+        (
+            "それは仕方がない",
+            "無い",
+            "ADJ",
+            # TODO 格助詞「が」も取得できるように
+            [Hitei],
+        ),
+        (
+            "あなたを愛したい",
+            "たい",
+            "AUX",
+            [KibouSelf],
+        ),
+        (
+            "子供が遊びたがる",
+            "たがる",
+            "AUX",
+            [KibouOthers],
+        ),
+        (
+            "とても歩いた",
+            "た",
+            "AUX",
+            [KakoKanryo],
+        ),
+        (
+            "とても走った",
+            "た",
+            "AUX",
+            [KakoKanryo],
+        ),
+        (
+            "とても遊んだ",
+            "た",
+            "AUX",
+            [KakoKanryo],
+        ),
+        # 「そう」は「様態」「伝聞」の意味を持つためテスト追加
+        (
+            "とても遊びそう",
+            "そう",
+            "AUX",
+            [Youtai],
+        ),
+        (
+            "とてもきそう",
+            "そう",
+            "AUX",
+            [Youtai],
+        ),
+        (
+            "とてもしそう",
+            "そう",
+            "AUX",
+            [Youtai],
+        ),
+        (
+            "とても重そう",
+            "そう",
+            "AUX",
+            [Youtai],
+        ),
+        # っぽい  PART    接尾辞-形容詞的
+        (
+            "とても保守的っぽそう",
+            "そう",
+            "AUX",
+            [Youtai],
+        ),
+        (
+            "とても困難そう",
+            "そう",
+            "AUX",
+            [Youtai],
+        ),
+        # 辞書にある「〜的」
+        (
+            "その発想は保守的そう",
+            "そう",
+            "AUX",
+            [Youtai],
+        ),
+        # 辞書にない「〜的」
+        (
+            "その発想はパリピ的そう",
+            "そう",
+            # ja_ginzaだとこうなっただけ。モデルに応じて変わるかも。
+            "ADV",
+            [Youtai],
+        ),
+        (
+            "とても遊ぶそう",
+            "そう",
+            "AUX",
+            [Denbun],
+        ),
+        (
+            "とてもくるそう",
+            "そう",
+            "AUX",
+            [Denbun],
+        ),
+        (
+            "とてもするそう",
+            "そう",
+            "AUX",
+            [Denbun],
+        ),
+        (
+            "とても重いそう",
+            "そう",
+            "AUX",
+            [Denbun],
+        ),
+        (
+            "とても保守的っぽいそう",
+            "そう",
+            "AUX",
+            [Denbun],
+        ),
+        (
+            "とても困難だそう",
+            "そう",
+            "AUX",
+            [Denbun],
+        ),
+        # 辞書にある「〜的」
+        (
+            "その発想は保守的だそう",
+            "そう",
+            "AUX",
+            [Denbun],
+        ),
+        # 辞書にない「〜的」
+        (
+            "パリピ的だそう",
+            "そう",
+            "AUX",
+            [Denbun],
+        ),
+        # NOTE: 明確に意味を判別できないケースが存在するが、テストケースには追加しない
+        #       たとえば「ご飯を食べたそう」において、
+        #       「すでにご飯を食べてきたそうだ（過去の「た」＋伝聞の「そうだ」）」か
+        #       「食べたいと思っていそうだ（希望の「たい」＋様態の「そうだ」）」の区別がつかない
+        #
+        #       文章全体の意味を成立させるうえで問題は発生しないためBugとはしないが、
+        #       意味を扱ううえでは問題となるため、改善の余地がある。
+        # (
+        #     "ご飯を食べたそう",
+        #     "そう",
+        #     "AUX",
+        #     [Union[Denbun, Youtai]],
+        # ),
+        (
+            "とても良いらしい",
+            "らしい",
+            "AUX",
+            [Suitei],
+        ),
+        (
+            "とても困難らしい",
+            "らしい",
+            "AUX",
+            [Suitei],
+        ),
+        (
+            "表彰するべき",
+            "べし",
+            "AUX",
+            [Touzen],
+        ),
+        (
+            "つくるべし",
+            "べし",
+            "AUX",
+            [Touzen],
+        ),
+        (
+            "遊ぶよう",
+            "よう",
+            "AUX",
+            [HikyoReizi],
+        ),
+        (
+            "とても良いよう",
+            "よう",
+            "AUX",
+            [HikyoReizi],
+        ),
+        (
+            "とても困難なよう",
+            "よう",
+            "AUX",
+            [HikyoReizi],
+        ),
+        # NOTE: 名詞として扱われる場合、「よう」がrootとなりappendantとしては
+        #       何も抽出されない
+        (
+            "まるで宝石のよう",
+            "よう",
+            "NOUN",
+            [],
+        ),
+        (
+            "遊んでる",
+            "てる",
+            "AUX",
+            [Keizoku],
+        ),
+        (
+            "笑ってる",
+            "てる",
+            "AUX",
+            [Keizoku],
+        ),
+        # TODO 複数ケースの追加
+        # (
+        #     # 「では」で「だ」が取れないように
+        #     "あんまり成功しなそう",
+        #     "無い",
+        #     "AUX",
+        #     [Hitei, 伝聞],
+        # ),
+        # (
+        #     # 「で」で「だ」が取れないように
+        #     "それは愛ではない",
+        #     "無い",
+        #     "AUX",
+        #     [Hitei],
+        # ),
+        # (
+        #     # 「て」で「た」が取れないように
+        #     "それは努力してない",
+        #     "ない",
+        #     "AUX",
+        #     [Hitei],
+        # ),
+    ],
+)
+def test_spacy_katsuyo_text_appendants_detector(
+    nlp_ja, spacy_appendants_detector, text, norm, pos, expected
+):
+    sent = next(nlp_ja(text).sents)
+    last_token = sent[-1]
+    assert last_token.norm_ == norm, "last token is not correct"
+    assert last_token.pos_ == pos, "last token is not correct"
+    appendants, has_error = spacy_appendants_detector.detect_from_sent(sent, sent.root)
+    assert not has_error, "has error in detection"
+    appendant_types = [type(appendant) for appendant in appendants]
+    assert appendant_types == expected
+
+
+@pytest.mark.parametrize(
+    "text, norm, tag, expected",
+    [
+        (
+            "あなたを愛するばかり",
+            "ばかり",
+            "助詞-副助詞",
+            [FUKUZYOSHI_BAKARI],
+        ),
+        (
+            "あなたを愛するばっかり",
+            "ばかり",
+            "助詞-副助詞",
+            [FUKUZYOSHI_BAKARI],
+        ),
+        (
+            "あなたを愛するまで",
+            "まで",
+            "助詞-副助詞",
+            [FUKUZYOSHI_MADE],
+        ),
+        (
+            "あなたを愛するだけ",
+            "だけ",
+            "助詞-副助詞",
+            [FUKUZYOSHI_DAKE],
+        ),
+        (
+            "あなたを愛するほど",
+            "ほど",
+            "助詞-副助詞",
+            [FUKUZYOSHI_HODO],
+        ),
+        (
+            "あなたを愛するくらい",
+            "くらい",
+            "助詞-副助詞",
+            [FUKUZYOSHI_KURAI],
+        ),
+        (
+            "あなたを愛するぐらい",
+            "くらい",
+            "助詞-副助詞",
+            [FUKUZYOSHI_KURAI],
+        ),
+        (
+            "あなたを愛するなど",
+            "など",
+            "助詞-副助詞",
+            [FUKUZYOSHI_NADO],
+        ),
+        # 「なり」を取得できる例文がない
+        # (
+        #     "あなたを信じるなり",
+        #     "なり",
+        #     "助詞-副助詞",
+        #     [FUKUZYOSHI_NARI],
+        # ),
+        (
+            "あなたを愛するやら",
+            "やら",
+            "助詞-副助詞",
+            [FUKUZYOSHI_YARA],
+        ),
+        (
+            "あなたを愛するかどうか",
+            "か",
+            "助詞-副助詞",
+            [FUKUZYOSHI_KA],
+        ),
+        (
+            "あなたを愛するのみ",
+            "のみ",
+            "助詞-副助詞",
+            [FUKUZYOSHI_NOMI],
+        ),
+        (
+            "あなたを愛するひとつずつ",
+            "ずつ",
+            "助詞-副助詞",
+            [FUKUZYOSHI_ZUTSU],
+        ),
+        (
+            "あなたに付きっきり",
+            "きり",
+            "助詞-副助詞",
+            [FUKUZYOSHI_KIRI],
+        ),
+        (
+            "あなたはひとりきり",
+            "きり",
+            "助詞-副助詞",
+            [FUKUZYOSHI_KIRI],
+        ),
+        (
+            "あなたを愛するの",
+            "の",
+            "助詞-終助詞",
+            [SHUJOSHI_NO],
+        ),
+        (
+            "あなたを愛するのに",
+            "に",
+            "助詞-格助詞",  # 特殊なケース
+            [SHUJOSHI_NONI],
+        ),
+        (
+            "その荷物は遠方のに",
+            "に",
+            "助詞-格助詞",  # 特殊なケース
+            [],
+        ),
+        (
+            "あなたを愛しちゃうな",
+            "な",
+            "助詞-終助詞",
+            [SHUJOSHI_NA],
+        ),
+        (
+            "あなたを愛するのか",
+            "か",
+            "助詞-終助詞",
+            [SHUJOSHI_KA],
+        ),
+        (
+            "あなたを愛するか",
+            "か",
+            "助詞-終助詞",
+            [SHUJOSHI_KA],
+        ),
+        (
+            "あなたを愛せるかしら",
+            "かしら",
+            "助詞-終助詞",
+            [SHUJOSHI_KASHIRA],
+        ),
+    ],
+)
+def test_spacy_shujoshi_appendants_detector(
+    nlp_ja, spacy_appendants_detector, text, norm, tag, expected
+):
+    sent = next(nlp_ja(text).sents)
+    last_token = sent[-1]
+    assert last_token.norm_ == norm, "last token is not correct"
+    assert last_token.tag_ == tag, "last token is not correct"
+    appendants, has_error = spacy_appendants_detector.detect_from_sent(sent, sent.root)
+    assert not has_error, "has error in detection"
+    assert appendants == expected
+
+
+@pytest.mark.parametrize(
+    "text, norm, pos",
+    [
+        (
+            "最高だ",
+            "だ",
+            "AUX",
+        ),
+        (
+            "最高です",
+            "です",
+            "AUX",
+        ),
+        (
+            "悲しみます",
+            "ます",
+            "AUX",
+        ),
+        (
+            "嫉妬しちゃう",
+            "ちゃう",
+            "AUX",
+        ),
+        (
+            "悲しんじゃう",
+            # 「ちゃう」になる
+            "ちゃう",
+            "AUX",
+        ),
+        (
+            "嫉妬しやがる",
+            "やがる",
+            "AUX",
+        ),
+    ],
+)
+def test_spacy_katsuyo_text_appendants_detector_ignore(
+    nlp_ja, spacy_appendants_detector, text, norm, pos
+):
+    sent = next(nlp_ja(text).sents)
+    last_token = sent[-1]
+    assert last_token.norm_ == norm, "last token is not correct"
+    assert last_token.pos_ == pos, "last token is not correct"
+    appendants, has_error = spacy_appendants_detector.detect_from_sent(sent, sent.root)
+    assert not has_error, "has error in detection"
+    assert appendants == [], f"{norm} will be ignored"
